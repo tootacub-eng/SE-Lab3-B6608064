630c96797775da8b17bb2db696af96c0c7cb0a43
tootacub-eng <tootacub@gmail.com>
2025-11-05 11:25:58 +0700

    ทำ Entity Student - close #6

---
+++ backend/entity/Student.go
@@ -0,0 +1,9 @@
+package models 
+import "gorm.io/gorm"
+type Student struct {
+	gorm.Model
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
+	Email     string `json:"email"`
+}
